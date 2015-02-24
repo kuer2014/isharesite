5157b105d71a813fe6d457d859b81a2417971a0c
kuer2014 <kuer-2006@163.com>
2015-02-24 23:38:47 +0800

    追加文件提交

---
+++ BetterSite.DataAccess/BaseDA.cs
@@ -0,0 +1,58 @@
+﻿using IBatisNet.DataMapper;
+using System.Collections.Generic;
+
+namespace BetterSite.DataAccess
+{
+    public static class BaseDA
+    {
+        public static int Insert<T>(string statementName, T t)
+        {
+            ISqlMapper iSqlMapper = Mapper.Instance();
+            if (iSqlMapper != null)
+            {
+                return (int)iSqlMapper.Insert(statementName, t);
+            }
+            return 0;
+        }
+
+        public static int Update<T>(string statementName, T t)
+        {
+            ISqlMapper iSqlMapper = Mapper.Instance();
+            if (iSqlMapper != null)
+            {
+                return iSqlMapper.Update(statementName, t);
+            }
+            return 0;
+        }
+
+        public static int Delete(string statementName, int primaryKeyId)
+        {
+            ISqlMapper iSqlMapper = Mapper.Instance();
+            if (iSqlMapper != null)
+            {
+                return iSqlMapper.Delete(statementName, primaryKeyId);
+            }
+            return 0;
+        }
+
+        public static T Get<T>(string statementName, int primaryKeyId) where T : class
+        {
+            ISqlMapper iSqlMapper = Mapper.Instance();
+            if (iSqlMapper != null)
+            {
+                return iSqlMapper.QueryForObject<T>(statementName, primaryKeyId);
+            }
+            return null;
+        }
+
+        public static IList<T> QueryForList<T>(string statementName, object parameterObject = null)
+        {
+            ISqlMapper iSqlMapper = Mapper.Instance();
+            if (iSqlMapper != null)
+            {
+                return iSqlMapper.QueryForList<T>(statementName, parameterObject);
+            }
+            return null;
+        }
+    }
+}
