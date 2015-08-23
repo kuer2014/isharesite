16372409f50913f859f11fd538595f7b1fdf1120
kuer2014 <kuer-2006@163.com>
2015-08-23 22:10:16 +0800

    主要修改了iBatis的配置方式发布后找不到config文件的问题

--- BetterSite.DataAccess/MapperHelper.cs
+++ BetterSite.DataAccess/MapperHelper.cs
@@ -20,23 +20,47 @@ namespace BetterSite.DataAccess
         {
             mapper = null;
         }
+        //protected static void InitMapper()
+        //{
+        //    ConfigureHandler handler = new ConfigureHandler(Configure);
+        //    DomSqlMapBuilder builder = new DomSqlMapBuilder();
+        //    string connString = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+        //    NameValueCollection prop = new NameValueCollection();
+        //    prop.Add("ConnectionString", connString);
+        //    builder.Properties = prop;
+        //   mapper = builder.ConfigureAndWatch(@"../BetterSite.DataAccess/SqlMap.config", handler);
+        //  //  mapper = builder.ConfigureAndWatch(@"F:\NavSite\SRC\BetterSite\BetterSite.DataAccess\SqlMap.config", handler);
+
+
+        //   // Assembly assembly = typeof(MapperHelper).Assembly;
+        //   // System.IO.Stream streamSmall = assembly.GetManifestResourceStream("BetterSite.DataAccess.SqlMap.config");
+        //   //mapper = builder.Configure(streamSmall);    //.ConfigureAndWatch(  "bin\\SQLMapConfig\\DEVBZZB.config",  handler);
+        //   // mapper.SessionStore = new HybridWebThreadSessionStore(mapper.Id);
+        //   // //mapper.DataSource.DbProvider.DbCommandTimeout = 900;
+        //}
+        /// <summary>
+        /// 初始化SqlMapper
+        /// </summary>
         protected static void InitMapper()
         {
             ConfigureHandler handler = new ConfigureHandler(Configure);
             DomSqlMapBuilder builder = new DomSqlMapBuilder();
-            string connString = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
-            NameValueCollection prop = new NameValueCollection();
-            prop.Add("ConnectionString", connString);
-            builder.Properties = prop;
-           mapper = builder.ConfigureAndWatch(@"../BetterSite.DataAccess/SqlMap.config", handler);
-          //  mapper = builder.ConfigureAndWatch(@"F:\NavSite\SRC\BetterSite\BetterSite.DataAccess\SqlMap.config", handler);
+            string connection;
+            NameValueCollection properties;
 
-
-           // Assembly assembly = typeof(MapperHelper).Assembly;
-           // System.IO.Stream streamSmall = assembly.GetManifestResourceStream("BetterSite.DataAccess.SqlMap.config");
-           //mapper = builder.Configure(streamSmall);    //.ConfigureAndWatch(  "bin\\SQLMapConfig\\DEVBZZB.config",  handler);
-           // mapper.SessionStore = new HybridWebThreadSessionStore(mapper.Id);
-           // //mapper.DataSource.DbProvider.DbCommandTimeout = 900;
+            connection = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+            //connection = ConfigurationManager.AppSettings["conn"];
+            // Put the string in collection to pass to the iBATIS configurator
+            properties = new NameValueCollection();
+            properties.Add("conn", connection);
+            // Build the iBATIS configurator
+            builder = new DomSqlMapBuilder();
+            builder.Properties = properties;
+            Assembly assembly = typeof(MapperHelper).Assembly;
+            System.IO.Stream streamSmall = assembly.GetManifestResourceStream(@"BetterSite.DataAccess.SqlMap.config");
+            mapper = builder.Configure(streamSmall);    //.ConfigureAndWatch(  "bin\\SQLMapConfig\\DEVBZZB.config",  handler);
+            mapper.SessionStore = new HybridWebThreadSessionStore(mapper.Id);
+           // mapper.DataSource.DbProvider.DbCommandTimeout = 900;
         }
         public static ISqlMapper Instance()
         {
--- BetterSite.UI/Global.asax.cs
+++ BetterSite.UI/Global.asax.cs
@@ -18,7 +18,7 @@ namespace BetterSite.UI
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+          //  WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
