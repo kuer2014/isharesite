f1c8c651ad496afa76bfaf8e993a55dd11d8db45
Administrator <Administrator@AMC5WGAO8ITHT59>
2017-06-26 18:57:37 +0800

    后台文章功能

---
+++ BetterSite.DataAccess/ArticleDA.cs
@@ -0,0 +1,47 @@
+﻿using BetterSite.Domain;
+using IBatisNet.DataMapper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BetterSite.DataAccess
+{
+  public  class ArticleDA
+    {
+      ///插入
+      public object Insert(M_Article model)
+      {
+          return BaseDA.Insert<M_Article>("Tab_Article_Insert", model);
+      }
+      ///更新
+      public int Update(M_Article model)
+      {
+          return BaseDA.Update<M_Article>("Tab_Article_Update", model);
+      }
+      ///删除
+      public int Delete(int id)
+      {
+          return BaseDA.Delete("Tab_Article_Delete", id+"");
+      }
+      ///查询对象
+      public M_Article Get(string siteId)
+      {
+          return BaseDA.Get<M_Article>("", siteId);
+      }
+      ///查询列表
+      public IList<M_Article> QueryForEntityList(M_Article where)
+      {
+          return BaseDA.QueryForEntityList<M_Article>("Tab_Article_SelectList", where);
+      }
+      public IList QueryForList(M_Article where)
+      {
+          return BaseDA.QueryForList("Tab_Article_Select", where);
+      }
+      public IList QueryForPageList(M_Article where)
+      {
+          return BaseDA.QueryForList("Tab_Article_SelectPageList", where);
+      }
+    }
+}
---
+++ BetterSite.UI/Areas/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,126 @@
+﻿using BetterSite.BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BetterSite.UI.Areas.Admin.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly ArticleBO articleBO = new ArticleBO();
+        //
+        // GET: /Admin/Article/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetAllEntitys(BetterSite.Domain.M_Article where)//, int page, int rows)
+        {
+            //  where.PageIndex = page;
+            // where.PageSize = rows;
+            var count = articleBO.QueryForList(where).Count;
+            var list = articleBO.QueryForPageList(where);
+            var data = new
+            {
+                total = count,
+                rows = list
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
+
+        }
+
+        //
+        // GET: /Admin/Article/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Create(BetterSite.Domain.M_Article entity)
+        {
+            JsonResult json = new JsonResult();
+            try
+            {
+                // TODO: Add insert logic here
+                articleBO.Insert(entity);
+                json.Data = new
+                {
+                    success = true,
+                    msg = "添加成功"
+                };
+
+            }
+            catch (Exception ex)
+            {
+                json.Data = new
+                {
+                    success = false,
+                    msg = ex.Message
+                };
+            }
+            return json;
+        }
+
+        //
+        // POST: /Admin/Types/Edit/5
+
+        [HttpPost]
+        public JsonResult Edit(BetterSite.Domain.M_Article entity)
+        {
+            JsonResult json = new JsonResult();
+            try
+            {
+                // TODO: Add insert logic here
+                articleBO.Update(entity);
+                json.Data = new
+                {
+                    success = true,
+                    msg = "修改成功"
+                };
+            }
+            catch (Exception ex)
+            {
+                json.Data = new
+                {
+                    success = false,
+                    msg = ex.Message
+                };
+            }
+            return json;
+        }
+
+        //
+        // POST: /Admin/Types/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(BetterSite.Domain.M_Article entity)
+        {
+            JsonResult json = new JsonResult();
+            try
+            {
+                // TODO: Add insert logic here
+                articleBO.Delete(entity.Id);
+                json.Data = new
+                {
+                    success = true,
+                    msg = "删除成功"
+                };
+
+            }
+            catch (Exception ex)
+            {
+                json.Data = new
+                {
+                    success = false,
+                    msg = ex.Message
+                };
+            }
+            return json;
+        }
+    }
+}
